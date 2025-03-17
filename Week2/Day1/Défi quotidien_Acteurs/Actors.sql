310649246ce4fcdd9b83262e7b42ded0bfc0ccf0
MohamedMeksi <miksimohamed@gmail.com>
2025-03-17 11:17:11 +0000

    Week2 Da

--- Week2/Day1/Défi quotidien_Acteurs/Actors.sql
+++ Week2/Day1/Défi quotidien_Acteurs/Actors.sql
@@ -1,3 +1,9 @@
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Matt','Damon','08/10/1970', 5);
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('George','Clooney','06/05/1961', 2);
+
 -- 1. Comptez le nombre d'acteurs dans la table.
 -- SELECT COUNT(*) FROM actors;
 
