049ca86946ac646afe0d9dbcb33e094da64259a4
MohamedMeksi <miksimohamed@gmail.com>
2025-04-02 09:39:35 +0000

    week 4 Day 1 A

---
+++ Week4/Day 1/EX3.js
@@ -0,0 +1,4 @@
+const isString = (value) => typeof value === 'string'
+
+console.log(isString('mohamed'))
+console.log(isString(3))
---
+++ Week4/Day 1/EX5.js
@@ -0,0 +1,13 @@
+
+function weight1 (kg) {return kg * 1000;}
+console.log(weight1(2));
+
+
+
+const weight2 = function(kg) {return kg * 1000};
+console.log(weight2(2));
+
+
+
+const weight3 = (kg) => kg * 1000;
+console.log(weight3(2));
