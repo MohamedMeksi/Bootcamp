e650a4d0546eada9fa01c2bb142c49769a0d088e
Mohamed Meksi <138502625+MohamedMeksi@users.noreply.github.com>
2025-04-14 10:04:44 +0100

    Delete Week5/Day 4/Exercices XP directory

--- Week5/Day 4/Exercices XP/EX2/app.js
+++
@@ -1,9 +0,0 @@
-import { personnes } from './data.js';
-
-function calculerAgeMoyen(personnes) {
-    const totalAge = personnes.reduce((acc, personne) => acc + personne.age, 0);
-    const ageMoyen = totalAge / personnes.length;
-    console.log(`L'âge moyen est de ${ageMoyen.toFixed(2)} ans.`);
-}
-
-calculerAgeMoyen(personnes);
--- Week5/Day 4/Exercices XP/EX2/data.js
+++
@@ -1,7 +0,0 @@
-// data.js
-export const personnes = [
-    { name: 'Alice', age: 25, location: 'Paris' },
-    { name: 'Bob', age: 32, location: 'Casablanca' },
-    { name: 'Charlie', age: 28, location: 'Rabat' },
-  ];
-  
