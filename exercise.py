a5261a70cb18440d72926e27190688615e16cb00
MohamedMeksi <miksimohamed@gmail.com>
2025-06-07 11:12:10 +0100

    1

---
+++ exercise.py
@@ -0,0 +1,19 @@
+def analyze_numbers(numbers):
+    """
+    Analyse une liste de nombres entiers.
+
+    Paramètres :
+        numbers (list): Une liste d'entiers.
+
+    Retourne :
+        dict: Un dictionnaire contenant :
+            - 'average': la moyenne des nombres
+            - 'even_count': le nombre de valeurs paires
+            - 'max': la plus grande valeur
+            - 'unique_sorted': une liste triée sans doublons
+
+    Exemple :
+    >>> analyze_numbers([2, 3, 4, 2, 7])
+    {'average': 3.6, 'even_count': 2, 'max': 7, 'unique_sorted': [2, 3, 4, 7]}
+    """
+    pass  # À compléter
